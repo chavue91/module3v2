5325df2c584706970ff103534985a94d54123b9f
Cha Vue <chavue09@gmail.com>
2022-11-08 00:20:41 -0600

    initial game setup 3.1.8

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var palyerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, palyerHealth, playerAttack);
 
@@ -10,6 +11,10 @@ var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
+    
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    if (promptFight === "fight" || promptFight === "FIGHT") {
 
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and us that result to update the value in the 'enemyHealth' variable
 
@@ -18,7 +23,7 @@ var fight = function() {
     //Log a resulting message to the console so we know that it worked
 
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." 
     );
 
     //Check enemy's health
@@ -39,7 +44,7 @@ var fight = function() {
     //Log a resulting message to the console so we know that it worked
 
     console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + palyerHealth + " health remaining."
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + palyerHealth + " health remaining and " + playerMoney + " gold."
     );
 
     //Check player health
@@ -50,6 +55,22 @@ var fight = function() {
 
     else {
         window.alert(playerName + " still has " + palyerHealth + " health left.");
+    }
+
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. " + playerName + " will lose 2 gold!");
+            playerMoney = playerMoney - 2;
+            console.log(
+                playerName + " has decided to skip this fight. " + playerName + " now have " + playerMoney + " gold left!"
+            );
+        }
+        else {
+            fight();
+        }
+    } else {
+        window.alert("You need to choose a valid option. Try again!");
     }
 
 
